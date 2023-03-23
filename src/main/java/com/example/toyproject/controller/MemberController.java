59c0290489713d5c59ca06cb600cf6859950eb91
suddiyo <suddii01@sju.ac.kr>
2023-03-23 11:46:53 +0900

    Fix: MemberController 변경사항 반영

--- src/main/java/com/example/toyproject/controller/MemberController.java
+++ src/main/java/com/example/toyproject/controller/MemberController.java
@@ -1,12 +1,18 @@
 package com.example.toyproject.controller;
 
+import com.example.toyproject.SessionConst;
 import com.example.toyproject.domain.Member;
 import com.example.toyproject.repository.BoardRepository;
 import com.example.toyproject.repository.MemberRepository;
+import com.example.toyproject.service.SignInService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +23,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class MemberController {
 
     private final MemberRepository memberRepository;
+    private final SignInService signInService;
 
     /**
      * sign-up
@@ -28,9 +35,11 @@ public class MemberController {
     }
 
     @PostMapping("/member/sign-up")
-    public String signUpMember(@ModelAttribute Member member, Model model) {
+    public String signUpMember(@Validated @ModelAttribute Member member, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "form/member/signUpForm";
+        }
         Member savedMember = memberRepository.save(member); // 회원가입
-        model.addAttribute("member", savedMember);
         return "redirect:sign-in";
     }
 
@@ -38,18 +47,26 @@ public class MemberController {
      * sign-in
      */
     @GetMapping("/member/sign-in")
-    public String signInForm(@ModelAttribute Member member, Model model) {
-        member.setPassword("");
-        model.addAttribute("member", member);
+    public String signInForm(@ModelAttribute("signInForm") SignInForm form) {
         return "form/member/signInForm";
     }
 
     @PostMapping("/member/sign-in")
-    public String signIn(@ModelAttribute Member member) {
-        Member findMember = memberRepository.findByEmail(member.getEmail());
-        // 로그인 검증 로직 추가 필요
-        // RedirectAttributes 이용하여 status, memberId 값 전달?
-        return "redirect:/board/" + findMember.getMemberId();
+    public String signIn(@Validated @ModelAttribute("signInForm") SignInForm form, BindingResult bindingResult, HttpServletRequest request) {
+        if (bindingResult.hasErrors()) {
+            return "form/member/signInForm";
+        }
+
+        // sign-in
+        Member signInMember = signInService.signIn(form.getEmail(), form.getPassword());
+        if (signInMember == null) {
+            bindingResult.reject("signInFail", "이메일 또는 비밀번호가 올바르지 않습니다.");
+            return "form/member/signInForm";
+        }
+        // 로그인 성공 세션 처리 (세션에 로그인 회원 정보 보관)
+        HttpSession session = request.getSession();
+        session.setAttribute(SessionConst.SIGN_IN_MEMBER, signInMember);
+        return "redirect:/";
     }
 
 }
