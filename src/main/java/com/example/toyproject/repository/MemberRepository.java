4d615a670326764cf0409fc07654bc9aa058ce23
suddiyo <suddii01@sju.ac.kr>
2023-03-30 20:55:58 +0900

    Fix: MemberRepository와 MemoryMemberRepository로 분리

--- src/main/java/com/example/toyproject/repository/MemberRepository.java
+++ src/main/java/com/example/toyproject/repository/MemberRepository.java
@@ -5,41 +5,12 @@ import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
-@Repository
-public class MemberRepository {
-    private static final Map<Long, Member> store = new HashMap<>(); //static
-    private static long sequence = 0L; //static
-
-    public Member save(Member member) {
-        member.setMemberId(++sequence);
-        store.put(member.getMemberId(), member);
-        return member;
-    }
-
-    public Member findByMemberId(Long memberId) {
-        return store.get(memberId);
-    }
-
-    public Optional<Member> findByEmail(String email) {
-        return findAll().stream()
-                .filter(m -> m.getEmail().equals(email))
-                .findFirst();
-    }
-
-    public List<Member> findAll() {
-        return new ArrayList<>(store.values());
-    }
-
-    public void update(Long memberId, Member updateParam) {
-        // 업데이트 전 유효성 검사 필요
-        Member findMember = findByMemberId(memberId);
-        findMember.setEmail(updateParam.getEmail());
-        findMember.setPassword(updateParam.getPassword());
-        findMember.setName(updateParam.getName());
-    }
-
-    public void clearStore() {
-        store.clear();
-    }
+public interface MemberRepository {
+    public Member save(Member member);
+    public Member findByMemberId(Long memberId);
+    public Optional<Member> findByEmail(String email);
+    public List<Member> findAll();
+    public void update(Long memberId, Member updateParam);
+    public void clearStore();
 }
 
---
+++ src/main/java/com/example/toyproject/repository/MemoryMemberRepository.java
@@ -0,0 +1,45 @@
+package com.example.toyproject.repository;
+
+import com.example.toyproject.domain.Member;
+import org.springframework.stereotype.Repository;
+
+import java.util.*;
+
+@Repository
+public class MemoryMemberRepository implements MemberRepository{
+    private static final Map<Long, Member> store = new HashMap<>(); //static
+    private static long sequence = 0L; //static
+
+    public Member save(Member member) {
+        member.setMemberId(++sequence);
+        store.put(member.getMemberId(), member);
+        return member;
+    }
+
+    public Member findByMemberId(Long memberId) {
+        return store.get(memberId);
+    }
+
+    public Optional<Member> findByEmail(String email) {
+        return findAll().stream()
+                .filter(m -> m.getEmail().equals(email))
+                .findFirst();
+    }
+
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    public void update(Long memberId, Member updateParam) {
+        // 업데이트 전 유효성 검사 필요
+        Member findMember = findByMemberId(memberId);
+        findMember.setEmail(updateParam.getEmail());
+        findMember.setPassword(updateParam.getPassword());
+        findMember.setName(updateParam.getName());
+    }
+
+    public void clearStore() {
+        store.clear();
+    }
+}
+
