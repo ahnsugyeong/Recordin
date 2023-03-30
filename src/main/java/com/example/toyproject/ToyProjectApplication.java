22f13ba5626a4a06766b83ca364e1b0fa99af30e
suddiyo <suddii01@sju.ac.kr>
2023-03-30 20:51:24 +0900

    Fix: Interceptor로 개인 페이지 접근 제한

--- src/main/java/com/example/toyproject/ToyProjectApplication.java
+++ src/main/java/com/example/toyproject/ToyProjectApplication.java
@@ -2,8 +2,9 @@ package com.example.toyproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class ToyProjectApplication {
 
 	public static void main(String[] args) {
